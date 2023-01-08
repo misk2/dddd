7fc69945a730b3aa1fb09db7deba5b4749f910f7
misk2 <117576468+misk2@users.noreply.github.com>
2023-01-08 16:47:15 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,163 +1,108 @@
 #include <stdio.h>
-
-
-/*
-SILNIA
-int silnia( int i)
-{
-    if(i<=1) {return 1;}
-    else{ return i*silnia(i-1);
-    }
+#include <stdlib.h>
+char tablica[20];
+int n=0;
+void wyswietlanie(){
+if(n==0) {printf("tablica jest pusta");}
+else {
+for(int i=0;i<n;i++){
+printf(tablica[i]);
 }
+}
+
+}
+void miejsce(){
+if(n<20) { printf("podaj liczbe");
+int ddd;
+scanf("%d", &ddd);
+if(ddd>0 || ddd<=256){
+tablica[n] = ddd;
+n++;
+printf("twoja liczba zostala dodana do tablicy");}
+else {
+printf("nie ma miejsca w tablicy");
+}
+
+
+}
+
+}
+void dodanie() {
+if(n>=20) {printf("tablica jest pelna");}
+else {
+printf("podaj znak ktory ma zostac dodany");
+char dd;
+scanf("%d", &dd);
+tablica[n] = dd;
+n++;
+
+}
+
+
+}
+/*void usuwanie(int k) {
+
+if (k>0 || k<=20){
+tablica[k]=tablica[k+1];
+}
+
+
+
+}*/
+int kwadrat(){
+int suma;
+if (n==0){printf("Tablica jest pusta");}
+else {
+for(int i=0;i<=n;i++){
+
+suma += tablica[i]*tablica[i];
+return suma;
+}
+}
+}
+
 int main()
 {
-    int a;
-    scanf("%d",&a);
-    printf("silnia wynosi : %d",silnia(a));
+ char x;
+ scanf("%c", &x);
 
-    return 0;
-}
+ switch(x)
+ {
+ case 'e':
+ break;
+ case 'w':
+ wyswietlanie();
+ break;
+case 'd':
+miejsce();
+ break;
+ case 'D':
+ dodanie();
+ break;
+ case 'u':
 
+ break;
+ case 's':
+ kwadrat(&n);
+ break;
+ case 't':
+ break;
+ case 'T':
+ break;
+ default:
+ break;
+
+
+ }
+
+
+
+/*printf("jaki indeks ma zostac usuniety?");
+int k;
+scanf("%d", &k);
+if (k>0 || k<=20){
+usuwanie();
+  }
+  else{printf("niepoprawny indeks");}
 */
-
-/* EUKLIDES
-int euklides(int a,int b){
-    if(b!=0)
-        { return euklides(b,a%b);
-    }
-else{
-    return a;}
-
-}
-int main() {
-int a,b;
-scanf("%d %d", &a,&b);
-printf("%d", euklides(a,b));
-
-}
-*/
-
-/* WYPIS
-int wypis(int a) {
-    if(a<2) {
-        return 1;
-    }
-    else {
-        printf("%d ", a);
-        return wypis(a-1);
-
-
-    }
-}
-int main() {
-    int a;
-    scanf("%d", &a);
-    printf("%d", wypis(a));
-
-}
-*/
-
-/* FIBONACCI
-
-int fibonacci(int a) {
-
-    if(a<2) {
-        return 1;
-    }
-    else return (fibonacci(a-1))+(fibonacci(a-2));
-}
-int main() {
-int a;
-scanf("%d" , &a);
-printf("%d", fibonacci(a));
-}
-
-*/
-
-/*BINARKA
-int binarka(a){
-if(a>0) {
-printf("%d", a%2);
-binarka(a/2);
-
-}
-}
-int main(){
-int a;
-scanf("%d", &a);
-printf("%d", binarka(a));
-}
-
-*/
-/*   suma cyfr liczby
-int b;
-int sumalicz(a){
-    if(a>0) {
-    b = b + a%10;
-    return sumalicz(a/10);
-    }
-    printf("%d", b);
-}
-int main(){
-int a;
-scanf("%d", &a);
-sumalicz(a);
-}
-
-
-*/
-
-/*ZADANIE 7A
-int i=0;
-int tablica[10] = {0,1,2,3,4,5,6,7,8,9};
-int spr(a) {
-if(a == tablica[i]){
-printf("Ta cyfra znajduje sie w tablicy");
-
-}
-else {if(i<11) {
-i++;
-return spr(a);
-
-    }
-else {
-    printf("Ta liczba nie znajduje sie w tablicy");
-    }
-}
-}
-int main(){
-int a;
-scanf("%d", &a);
-spr(a);
-
-
-}
-
-*/
-int x=0, y=10, z;
-int tablica[10] = {0,1,2,3,4,5,6,7,8,9};
-int sprbinarne(a){
-z=(x+y)/2;
-if(y-x==1) {
-    printf("liczba ta nie znajduje się w tablicy");
-    return 0;
-}
-if(a==tablica[z]) {
-     printf("twoja liczba znajduje się w tablicy");
-     return 0;
-
-}
-else if (a>tablica[z]) {
-        x=z;
-    return sprbinarne(a);
-}
-else {
-    y=z;
-    return sprbinarne(a);
-}
-}
-int main() {
-int a;
-scanf("%d", &a);
-sprbinarne(a);
 }
